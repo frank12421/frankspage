601fb749fb13611047b67584b055dca20bea327e
Frank Böker <boeker.frank@gmail.com>
2023-04-08 05:33:13 +0200

    add Character Postion Change by click

--- js/index.js
+++ js/index.js
@@ -1,43 +1,44 @@
 const body = document.querySelector("body");
-// console.log(body);
-
 const myName = "frank";
-
 const container = document.createElement("h1");
-//container.style.color = `hsl(${count} 80% 40%)`;
-//container.innerText = "frank";
 container.classList.add("animation");
-// console.log(container);
-//disintegratName(myName);
 container.append(disintegratName(myName));
 body.append(container);
 
 function disintegratName(myName) {
   const nameLength = myName.length;
+  const myArray = myName.split("");
+  // console.log(myArray);
   const container = document.createElement("div");
   container.classList.add("container__characters");
   container.setAttribute("data-js", "containerCharacters");
-  //  console.log(nameLength);
-  for (i = 0; i < myName.length; i++) {
-    // console.log(myName.at(i));
-    const myLetter = myName.at(i);
-    const myElement = document.createElement("div" + i);
-    myElement.innerText = myLetter;
-    myElement.classList.add("single__letter");
-    myElement.addEventListener("mouseover", (event) => {
-      console.log(`Over: ${myLetter}`);
-    });
-    myElement.addEventListener("click", (event) => {
-      console.log(`click: ${myLetter}`);
-      letterClick_NewSort();
-    });
-    console.log(myElement);
-    container.append(myElement);
-  }
+  myArray.forEach((element, index) => {
+    console.log(index);
+    container.append(styleCharacter(element, index));
+  });
   return container;
 }
 
-function letterClick_NewSort() {
-  const myDiv = document.querySelector(`[data-js="containerCharacters"]`);
-  console.log.apply(myDiv);
+function styleCharacter(value, index) {
+  const myElement = document.createElement("div");
+  myElement.innerText = value;
+  myElement.classList.add("single__letter");
+  myElement.id = index;
+  // myElement.addEventListener("mouseover", (event) => {});
+  myElement.addEventListener("click", (event) => {
+    clickCharacter(index);
+  });
+  return myElement;
+}
+
+function clickCharacter(index) {
+  const resetFirstElement = document.getElementsByClassName(
+    "single__letter__first"
+  );
+  if (resetFirstElement.length > 0) {
+    // console.log(resetFirstElement[0]);
+    resetFirstElement[0].classList.remove("single__letter__first");
+  }
+  const setFirstElement = document.getElementById(index);
+  setFirstElement.classList.add("single__letter__first");
 }
